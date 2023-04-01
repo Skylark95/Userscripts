3475ca71d35771e26a2819b7a734fe2d43eb8b77
Derek Cochran <derek@derekcochran.dev>
2023-04-01 13:06:01 -0500

    add keyboard shortcut to gg.deals

--- ggdeals.user.js
+++ ggdeals.user.js
@@ -2,12 +2,22 @@
 // @name        GG.Deals Steam Links
 // @namespace   https://github.com/Skylark95/Userscripts
 // @match       https://gg.deals/bundle/*
+// @match       https://gg.deals/deal/*
 // @grant       GM_xmlhttpRequest
 // @version     1.0
 // @author      Derek Cochran
 // @description Changes game links to steam on bundle pages
+// @require https://cdn.jsdelivr.net/npm/@violentmonkey/shortcut@1
 // ==/UserScript==
 function ggdeals() {
+    // Shortcuts
+    VM.shortcut.register('/', () => {
+        const input = document.querySelector('#global-search-input');
+        if (input) {
+            input.focus();
+        }
+    });
+    // Steam Links
     const items = document.querySelectorAll('.game-item');
     for (const item of items) {
         const title = item.querySelector('.game-info-wrapper .game-info-title-wrapper a');
