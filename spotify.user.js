9d1d8cffc91a8bbd63501513ea8a3fb449433b83
Derek Cochran <derek@derekcochran.dev>
2023-04-01 10:20:09 -0500

    add userscripts for spotify and ggdeals

---
+++ spotify.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        Spotify
+// @namespace   https://github.com/Skylark95/Userscripts
+// @match       https://open.spotify.com/*
+// @grant       GM_addStyle
+// @version     1.0
+// @author      Derek Cochran
+// @description Personal style changes for Spotify
+// ==/UserScript==
+GM_addStyle(`
+  .fyaNJr {
+    font-size: 1rem !important
+  }
+`);
---
+++ spotify.user.ts
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        Spotify
+// @namespace   https://github.com/Skylark95/Userscripts
+// @match       https://open.spotify.com/*
+// @grant       GM_addStyle
+// @version     1.0
+// @author      Derek Cochran
+// @description Personal style changes for Spotify
+// ==/UserScript==
+
+GM_addStyle(`
+  .fyaNJr {
+    font-size: 1rem !important
+  }
+`);
